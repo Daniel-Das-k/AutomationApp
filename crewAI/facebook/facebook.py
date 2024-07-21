c70890685a86575cef32608d7e0b1a78846bd246
Daniel-Das-k <143825117+Daniel-Das-k@users.noreply.github.com>
2024-07-21 23:18:55 +0530

    Update facebook.py

--- crewAI/facebook/facebook.py
+++ crewAI/facebook/facebook.py
@@ -23,32 +23,47 @@
 #     return "You are {name} on Facebook".format(name=resp.json()["name"])
 
 # if __name__ == "__main__":
-#     app.run()
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
 
-import os
-from flask import Flask, redirect, url_for
-from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+
+from flask import Flask, url_for,render_template, request,session,redirect
+import requests
+import os 
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
-app.config["FACEBOOK_OAUTH_CLIENT_ID"] = os.environ.get("FACEBOOK_OAUTH_CLIENT_ID")
-app.config["FACEBOOK_OAUTH_CLIENT_SECRET"] = os.environ.get("FACEBOOK_OAUTH_CLIENT_SECRET")
-app.config["FACEBOOK_OAUTH_REDIRECT_URI"] = "https://facebook-rakb.onrender.com"
-facebook_bp = make_facebook_blueprint(
-    client_id=app.config["FACEBOOK_OAUTH_CLIENT_ID"],
-    client_secret=app.config["FACEBOOK_OAUTH_CLIENT_SECRET"],
-    redirect_to="facebook.login"
-)
-app.register_blueprint(facebook_bp, url_prefix="/login")
+app.secret_key = "secret"
+Fb_APP_ID = "1519269175630468"
+Fb_APP_SECRET = "e04ebcfe52882ed9a9134937547e0ba0"
+Fb_REDIRECT_URI = "https://facebook-rakb.onrender.com/facebook/callback"
 
-@app.route("/")
-def index():
-    if not facebook.authorized:
-        return redirect(url_for("facebook.login"))
-    resp = facebook.get("/me")
-    assert resp.ok, resp.text
-    return "You are {name} on Facebook".format(name=resp.json()["name"])
+@app.route('/')
+def home():
+    return "home Page"
 
+@app.route('/facebook/login')
+def facebook_login():
+    print("Facebook login route accessed")
+    return redirect(f"https://www.facebook.com/v12.0/dialog/oauth?client_id={Fb_APP_ID}&redirect_uri={Fb_REDIRECT_URI}&scope=read_insights")
+
+@app.route('/facebook/callback')
+def facebook_callback():
+    print("Facebook callback route accessed")
+    code = request.args.get("code")
+    if code :
+        response = requests.get(
+            f"https://graph.facebook.com/v12.0/oauth/access_token?client_id={Fb_APP_ID}&redirect_uri={Fb_REDIRECT_URI}&client_secret={Fb_APP_SECRET}&code={code}"
+        )
+        data = response.json()
+        if "access_token" in data :
+            session["acces_token"] = data["access_token"]
+
+            user_response = requests.get(
+                f"https://graph.facebook.com/v12.0/me?fields=id,name,email&access_token=(data['access_token])"
+            )
+            print("User response",user_response)
+            return redirect('/')
+        
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
