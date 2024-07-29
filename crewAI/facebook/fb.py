23435e3f072e4afb740c65372689f94bc523adc3
Daniel-Das-k <143825117+Daniel-Das-k@users.noreply.github.com>
2024-07-29 21:28:12 +0530

    updated

--- crewAI/facebook/fb.py
+++ crewAI/facebook/fb.py
@@ -27,7 +27,7 @@ def home():
 
 @app.route('/facebook/login')
 def facebook_login():
-    return redirect(f"https://www.facebook.com/v12.0/dialog/oauth?client_id={Fb_APP_ID}&redirect_uri={Fb_REDIRECT_URI}&scope=read_insights")
+    return redirect(f"https://www.facebook.com/v12.0/dialog/oauth?client_id={Fb_APP_ID}&redirect_uri={Fb_REDIRECT_URI}&scope=read_insights,publish_video,pages_manage_instant_articles,pages_show_list,pages_messaging,instagram_basic,instagram_manage_comments,instagram_manage_insights,instagram_content_publish,page_events,pages_read_engagement,pages_manage_metadata,pages_read_user_content,pages_manage_posts,pages_manage_engagement,instagram_manage_events")
 
 @app.route('/facebook/callback')
 def facebook_callback():
@@ -61,3 +61,45 @@ def facebook_callback():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5002))
     app.run(host='0.0.0.0', port=port)
+
+# import facebook
+# import json
+# from datetime import datetime, timedelta
+# import time
+
+# class FacebookManager:
+#     def __init__(self):
+#         self.credentials = self._load_credentials()
+#         self.facebook_api = self._initialize_facebook_api()
+
+#     def _load_credentials(self):
+#         with open('facebook_token.json', 'r') as file:
+#             return json.load(file)
+        
+
+#     def _initialize_facebook_api(self):
+#         access_token = self.credentials['access_token']
+#         print(access_token)
+#         return facebook.GraphAPI(access_token)
+
+#     def schedule_post(self, content: str, datetime_str: str):
+#         post_time = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M')
+#         current_time = datetime.now()
+
+#         delay = (post_time - current_time).total_seconds()
+#         if delay > 0:
+#             time.sleep(delay)
+
+#         self._post_to_facebook(content)
+
+#     def _post_to_facebook(self, content: str):
+#         try:
+#             print("Posting facebook....")
+#             post = self.facebook_api.put_object(parent_object='me', connection_name='feed', message=content)
+#             print(post)
+#         except facebook.GraphAPIError as e:
+#             print(f"An error occurred: {e}")
+
+# if __name__ == "__main__":
+#     facebook_manager = FacebookManager()
+#     facebook_manager._post_to_facebook('Excited to announce our new project!')
